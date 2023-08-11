2b86029f7410e084fa6d2a41c3b58740799dab21
kznm <khaingmaw@Khaings-MacBook-Air.local>
2023-08-12 03:46:57 +0900

    how to add page transition

--- src/pages/Layouts/Layout.jsx
+++ src/pages/Layouts/Layout.jsx
@@ -1,16 +1,25 @@
 import React from "react";
-import { Outlet } from "react-router";
+import { Outlet ,useLocation} from "react-router";
 import Navbar from "../../components/Navbar";
-
+import { SwitchTransition, CSSTransition } from "react-transition-group";
+import './styles.css'
 const Layout = () => {
+  
+   const location= useLocation()
+      console.log(location.pathname);
+
   return (
     <div>
-      
-      <Navbar/>
+      <Navbar />
       {/* dynamic route changes content */}
-     <div className="max-w-6xl mx-auto p-3">
-     <Outlet />
-     </div>
+
+      <SwitchTransition>
+        <CSSTransition timeout={200} classNames='fade' key={location.pathname} >
+          <div className="max-w-6xl mx-auto p-3">
+            <Outlet />
+          </div>
+        </CSSTransition>
+      </SwitchTransition>
     </div>
   );
 };
