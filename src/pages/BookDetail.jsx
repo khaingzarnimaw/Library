0776a645a0838e79b146b97947010056c0572e73
kznm <khaingmaw@Khaings-MacBook-Air.local>
2023-08-13 13:57:05 +0900

    Book-

--- src/pages/BookDetail.jsx
+++ src/pages/BookDetail.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import useFetch from "../hooks/useFetch";
-
+import shin1img from "../assets/img/shin1.jpeg"
 const BookDetail = () => {
   //dynamic id//id ကိုလက်ခံဖို့ //useParams ကိုသုံးကြစို့
   let { id } = useParams();
@@ -16,7 +16,23 @@ let { data:book , loading, error} = useFetch(`http://localhost:3001/books/${id}`
        {error && <p>{error}</p>}
        {loading && <p>loading...</p>}
        {book && (
-         <h1>{book.title}</h1>
+         <div className="grid grid-cols-2">
+          <div>
+            <img src={shin1img} alt="" className=" w-[60%]"/>
+          </div>
+          <div className=" space-y-4">
+            <h1 className="text-3xl font-bold">{book.title}</h1>
+            <div className="space-x-3">
+               {book.categories.map(category => (
+                <span className="bg-blue-500 text-white rounded-full text-sm px-2 py-1" key={category}>{category}</span>
+               ))
+              }
+            </div>
+            <p>
+              {book.description}
+            </p>
+          </div>
+         </div>
        )}
    </>
   )
